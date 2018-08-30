575d6d72fa5eb7ba469d1eef1ecfcb141824552d
Jennifer <32855060+jennnico@users.noreply.github.com>
2018-08-30 17:57:00 -0400

    Y-axis corrected!
    -Months/ticks appear halfway up each bar (not at the base)
    -Axis is long enough!

--- index.js
+++ index.js
@@ -5,22 +5,24 @@ document.addEventListener('DOMContentLoaded',function(){
   req.onload=function(){
     json=JSON.parse(req.responseText);
     variance = json.monthlyVariance
+
+//1. Define the variables and modify the data array.
     
     // Parse the month and create a new data array
-    var data = []
-    var parseMonth = d3.timeParse('%m')
+    const data = []
+    const parseMonth = d3.timeParse('%m')
     variance.forEach(function(val){
       data.push([val.year, parseMonth(val.month), val.variance, val.month])
     })
-    const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    var legend = [2.8, 3.9, 5.0, 6.1, 7.2, 8.3, 9.5, 10.6, 11.7, 12.8, 13.9]
+    //Array of intervals for key/colors
+    const legend = [2.8, 3.9, 5.0, 6.1, 7.2, 8.3, 9.5, 10.6, 11.7, 12.8, 13.9]
     
     // X-axis indices based on year
     for (var i=0; i<data.length; i++){
       data[i].push(data[i][0] - 1752)
     }
       
-    // Set the dimensions of the canvas / graph
+    // Set the dimensions of the canvas, relative to the data
     const w = (data.length)/3 + 200
     const h = 12*40 + 200
     const padding = 100
@@ -30,40 +32,33 @@ document.addEventListener('DOMContentLoaded',function(){
                      .domain([d3.min(data, d => d[0]), d3.max(data, d => d[0])])  
                      .range([0, (w - (2*padding) -3)]);
     
-//1. Here, months are not centered.
     // Scale for y-axis (months)
-    /*const yScale = d3.scaleTime()
+    const yScale = d3.scaleTime()
                      .domain([d3.max(data, d => d[1]), d3.min(data, d => d[1])])
-                     .range([(h - (2*padding)), 40])*/
-
-//2. Here, axes don't meet. 
-    // Scale for y-axis (months)
-    const yScale = d3.scaleOrdinal()
-    .domain(months)
-    .range(months.map((month, i)=> {
-      return i * 40 + 20
-    }))
+                     .range([(h - (2*padding)), 40])
     
     // Define the axes
     const xAxis = d3.axisBottom(xScale)
-                    .tickFormat(d3.format("d")) //remove commas from years
+                    .tickFormat(d3.format("d")) //removes commas from years
+ 
     const yAxis = d3.axisLeft(yScale)
-                   // .tickFormat(d3.timeFormat("%B"));  //use with y-scale #1
-                   .tickFormat(month => { //use with y-scale #2
-                     return d3.timeFormat("%B")(parseMonth(month))
-                    });
-
+                    .tickFormat(d3.timeFormat("%B")); 
     
     // Define the div for the tooltip
-    var div = d3.select("#graph").append("div")	
+    const div = d3.select("#graph").append("div")	
                 .attr("class", "tooltip")				
                 .style("opacity", 0);
     
+    
+//2. Create the legend.
+    
+    //Add the legend canvas.
     const svgLegend=d3.select("#legend")
                       .append("svg")
                       .attr("width", 600)
                       .attr("height", 150)
     
+    //Add the legend rectangles.
     svgLegend.selectAll("rect")
              .data(legend)
              .enter()
@@ -74,7 +69,7 @@ document.addEventListener('DOMContentLoaded',function(){
              .attr("x", (d, i) => {return i*40})
              .attr("y", 1)
              .style('fill', (d) => {
-                  if (d <= 2.8){ //2.8
+                  if (d <= 2.8){ //2.8, navy
                     return ("navy")
                   }else if (d <= 3.9){ //3.9, dark blue
                     return ('#0057b5')
@@ -92,13 +87,14 @@ document.addEventListener('DOMContentLoaded',function(){
                     return ("#f9c425")
                   } else if (d <= 11.7){ //11.7, dark orange
                     return ("darkorange")
-                  } else if (d <= 12.8){
+                  } else if (d <= 12.8){ //12.8, red
                     return("red")
-                  } else {
+                  } else { //Over 12.8, dark red
                     return ("darkred")
                   }
                 })
     
+    //Add the legend text
     svgLegend.selectAll("text")
              .data(legend)
              .enter()
@@ -106,13 +102,14 @@ document.addEventListener('DOMContentLoaded',function(){
              .attr("x", (d, i) => i*40 + 30 )
              .attr("y", (d, i) => 60)
              .text(d=>d.toFixed(1))
+    
+//3. Creat the Heat Map
 
     // Adds the svg canvas
     const svg=d3.select("#graph")
                 .append("svg")
                 .attr("width", w)
                 .attr("height", h)
-                //.style("outline", "thin solid black")
     
     // Add the rectangles
     svg.selectAll("rect")
@@ -124,7 +121,7 @@ document.addEventListener('DOMContentLoaded',function(){
                 .attr("x", (d, i) => {return i = (d[4]*4 + padding - 3)})
                 .attr("y", (d, i) => {return d[3]*40 + 60})
                 .style('fill', (d) => {
-                  if (d[2] < -5.86){ //2.8
+                  if (d[2] < -5.86){ //2.8, navy
                     return ("navy")
                   }else if (d[2] < -4.76){ //3.9, dark blue
                     return ('#0057b5')
@@ -144,32 +141,32 @@ document.addEventListener('DOMContentLoaded',function(){
                     return ("darkorange")
                   } else if (d[2] < 4.14) { //12.8, red
                     return ("red")
-                  } else{
+                  } else{ //Over 12.8, dark red
                     return ("darkred")
                   }
                 })
        // Tooltip appears on hover
        .on("mouseover", function(d) {	
-      d3.select(this)
-        .style("outline", "thin solid black")
-      //modified width and height so outline would display properly
-        .style("width", 3)
-        .style("height", 39)
-           div.transition()		
-               .duration(200)		
-               .style("opacity", .8);		
-           div .html(d[0] + ": " + d3.timeFormat("%B")(d[1])  + " <br/>" +  (8.66+d[2]).toFixed(2) + String.fromCharCode(176) + "C" + " <br/>" + "Variance: " + d[2] + String.fromCharCode(176))	
-               .style("left", (d3.event.pageX + 10) + "px")		
-               .style("top", (d3.event.pageY - 28) + "px");
+         d3.select(this)
+           .style("outline", "thin solid black")
+           //Outline seemed obscured. Made rectangles smaller on hover to address this.
+           .style("width", 3)
+           .style("height", 39)
+         div.transition()		
+            .duration(200)		
+            .style("opacity", .8);		
+         div.html(d[0] + ": " + d3.timeFormat("%B")(d[1])  + " <br/>" +  (8.66+d[2]).toFixed(2) + String.fromCharCode(176) + "C" + " <br/>" + "Variance: " + d[2] + String.fromCharCode(176))	
+            .style("left", (d3.event.pageX + 10) + "px")		
+            .style("top", (d3.event.pageY - 28) + "px");
        })	
        .on("mouseout", function(d) {	
-          d3.select(this)
-            .style("outline", "thin solid transparent")
-            .style("width", 4)
-            .style("height", 40)
-           div.transition()		
-               .duration(500)		
-               .style("opacity", 0);	
+         d3.select(this)
+           .style("outline", "thin solid transparent")
+           .style("width", 4)
+           .style("height", 40)
+         div.transition()		
+            .duration(500)		
+            .style("opacity", 0);	
        });
 
 
@@ -187,7 +184,7 @@ document.addEventListener('DOMContentLoaded',function(){
  
     // Add and move the Y-axis
     svg.append("g")
-       .attr("transform", "translate(" + (padding) + ", " + (padding) + ")")
+       .attr("transform", "translate(" + (padding) + ", " + (padding - 20) + ")")
        .call(yAxis)
        .append("text")
        .attr("class", "label")
@@ -197,6 +194,15 @@ document.addEventListener('DOMContentLoaded',function(){
        .attr("dy", ".75em")
        .attr("transform", "rotate(-90)")
        .text("Months")
+    
+    //Y-axis not long enough, so I added a line.
+    //Resource: https://stackoverflow.com/questions/48027481/d3-js-extend-bottom-axis-line-to-origin
+        svg.append('line').classed('connecting-line', true)
+          .attr('y1', h-padding)
+          .attr('y2', padding)
+          .attr('x1', padding)
+          .attr('x2', padding)
+          .style('stroke', 'black')
     
   }
 })
